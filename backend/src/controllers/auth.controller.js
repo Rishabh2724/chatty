09e748b2bff82bf8de9ed37b290df977aa577d63
Rishabh2724 <23240@iiitu.ac.in>
2025-05-17 10:26:48 +0530

    fixing name error

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcryptjs";
-import User from "../models/user.Model.js"
+import User from "../models/user.model.js"
 import { generateToken } from "../lib/utils.js";
 import cloudinary from "../lib/cloudinary.js";
 
